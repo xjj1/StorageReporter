643b4920241039acbce5478b44195f712b94f566
Todor Todorov <ttodoroff@gmail.com>
2020-12-07 12:44:20 +0200

    cleanup

--- connector/connector.go
+++ connector/connector.go
@@ -1,9 +1,6 @@
 package connector
 
 import (
-	"errors"
-
-	"github.com/xjj1/StorageReporter/connector/sshcon"
 	"github.com/xjj1/StorageReporter/devices"
 )
 
@@ -11,12 +8,4 @@ type Connector interface {
 	Connect(a *devices.Device) error
 	ExecCmd(string) (string, error)
 	Close()
-}
-
-func Connect(a devices.Device) error {
-	if a.Type >= devices.UNKNOWN && a.Type <= devices.PURESTORAGE {
-		return sshcon.NewSSH().Connect(&a)
-	}
-
-	return errors.New("Can't connect, unknown device type")
 }
